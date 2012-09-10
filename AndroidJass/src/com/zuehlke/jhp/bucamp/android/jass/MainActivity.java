776662e05a8e9d22ede25c9407881696a466db73
fabiropr <fropraz@gmail.com>
2012-09-10 17:44:11 +0200

    hand and table fragments added

--- AndroidJass/src/com/zuehlke/jhp/bucamp/android/jass/MainActivity.java
+++ AndroidJass/src/com/zuehlke/jhp/bucamp/android/jass/MainActivity.java
@@ -11,21 +11,21 @@ import ch.mbaumeler.jass.core.JassEngine;
 import ch.mbaumeler.jass.core.Match;
 import ch.mbaumeler.jass.core.card.Card;
 import ch.mbaumeler.jass.core.game.PlayerToken;
+import ch.mbaumeler.jass.extended.ui.ObservableGame;
 
 public class MainActivity extends Activity {
+
+	private ObservableGame game;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		TextView textView = (TextView) findViewById(R.id.helloWorld);
-		JassEngine jassEngine = new JassEngine();
-		Game game = jassEngine.createJassGame();
-		Match currentMatch = game.getCurrentMatch();
-		PlayerToken activePlayer = currentMatch.getActivePlayer();
-		List<Card> cards = currentMatch.getCards(activePlayer);
-		textView.setText("Active Player " + activePlayer + " with cards: "
-				+ cards);
+		game = new ObservableGame(new JassEngine().createJassGame());
+	}
+
+	public ObservableGame getGame() {
+		return game;
 	}
 
 	@Override
