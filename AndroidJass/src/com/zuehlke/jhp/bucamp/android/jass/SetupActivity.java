955411a526e35d754ecd1a1d1d96efe989e9f49a
Stephan Christen <stephan.christen.business@gmail.com>
2012-09-12 08:53:25 +0200

    game settings UI

---
+++ AndroidJass/src/com/zuehlke/jhp/bucamp/android/jass/SetupActivity.java
@@ -0,0 +1,88 @@
+package com.zuehlke.jhp.bucamp.android.jass;
+
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.os.Bundle;
+
+import com.zuehlke.jhp.bucamp.android.jass.settings.model.JassSettings;
+import com.zuehlke.jhp.bucamp.android.jass.settings.model.ModelFactory;
+
+public class SetupActivity extends Activity {
+	
+	public static final String KEY_SETTINGS = "settings";
+	public static Context appContext;
+	
+	private JassSettings settings;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.setup_activity);
+
+		if( savedInstanceState != null && savedInstanceState.containsKey(KEY_SETTINGS)) {
+			settings = (JassSettings)savedInstanceState.get(KEY_SETTINGS);
+		}
+		else {
+			settings = ModelFactory.createDefaultModel();
+		}
+			
+		
+		// ActionBar gets initiated
+		ActionBar actionbar = getActionBar();
+
+		// Tell the ActionBar we want to use Tabs.
+		actionbar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+		// initiating all tabs and set text to it.
+		ActionBar.Tab tabSettings = actionbar.newTab().setText(
+				getResources().getString(R.string.setup_actionbar_settings));
+		ActionBar.Tab tabTeam1 = actionbar.newTab().setText(
+				getResources().getString(R.string.setup_actionbar_team1));
+		ActionBar.Tab tabTeam2 = actionbar.newTab().setText(
+				getResources().getString(R.string.setup_actionbar_team2));
+
+		// create the two fragments we want to use for display content
+		Fragment fragmentSettings = new SetupSettingsFragment();
+		fragmentSettings.setArguments(SetupSettingsFragment.createArguments(settings));
+		Fragment fragmentTeam1 = new SetupTeamFragment();
+		fragmentTeam1.setArguments(SetupTeamFragment.createArguments(settings.getTeam1())); 
+		Fragment fragmentTeam2 = new SetupTeamFragment();
+		fragmentTeam2.setArguments(SetupTeamFragment.createArguments(settings.getTeam2()));
+
+		// set the Tab listener. Now we can listen for clicks.
+		tabSettings.setTabListener(new SetupTabsListener(fragmentSettings));
+		tabTeam1.setTabListener(new SetupTabsListener(fragmentTeam1));
+		tabTeam2.setTabListener(new SetupTabsListener(fragmentTeam2));
+
+		// add the all tabs to the actionbar
+		actionbar.addTab(tabSettings);
+		actionbar.addTab(tabTeam1);
+		actionbar.addTab(tabTeam2);
+	}
+
+	class SetupTabsListener implements ActionBar.TabListener {
+		public Fragment fragment;
+
+		public SetupTabsListener(Fragment fragment) {
+			this.fragment = fragment;
+		}
+
+		public void onTabReselected(Tab tab, FragmentTransaction ft) {
+			// do nothing
+		}
+
+		public void onTabSelected(Tab tab, FragmentTransaction ft) {
+			ft.replace(R.id.fragment_container, fragment);
+		}
+
+		public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+			ft.remove(fragment);
+		}
+
+	}
+
+}
---
+++ AndroidJass/src/com/zuehlke/jhp/bucamp/android/jass/settings/model/JassSettings.java
@@ -0,0 +1,70 @@
+package com.zuehlke.jhp.bucamp.android.jass.settings.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class JassSettings implements Parcelable {
+
+	private long targetPoints;
+	private Team team1;
+	private Team team2;
+
+	public JassSettings(long targetPoints, Team team1, Team team2) {
+		super();
+		this.targetPoints = targetPoints;
+		this.team1 = team1;
+		this.team2 = team2;
+	}
+
+	public long getTargetPoints() {
+		return targetPoints;
+	}
+
+	public void setTargetPoints(long targetPoints) {
+		this.targetPoints = targetPoints;
+	}
+
+	public Team getTeam1() {
+		return team1;
+	}
+
+	public void setTeam1(Team team1) {
+		this.team1 = team1;
+	}
+
+	public Team getTeam2() {
+		return team2;
+	}
+
+	public void setTeam2(Team team2) {
+		this.team2 = team2;
+	}
+
+	public JassSettings(Parcel p) {
+		targetPoints = p.readLong();
+		team1 = new Team(p);
+		team2 = new Team(p);
+	}
+	
+	public int describeContents() {
+		return 0;
+	}
+
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeLong(targetPoints);
+		team1.writeToParcel(dest, flags);
+		team2.writeToParcel(dest, flags);
+	}
+	
+    public static final Parcelable.Creator<JassSettings> CREATOR =
+        	new Parcelable.Creator<JassSettings>() {
+                public JassSettings createFromParcel(Parcel in) {
+                    return new JassSettings(in);
+                }
+     
+                public JassSettings[] newArray(int size) {
+                    return new JassSettings[size];
+                }
+            };		
+
+}
